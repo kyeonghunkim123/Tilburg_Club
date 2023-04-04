ccd42bea8056b0c064f3d70c46f595799761489d
Kipyo <kipyobae1229@gmail.com>
2023-04-04 21:26:06 +0900

    - find_pw_update

--- userapp/views.py
+++ userapp/views.py
@@ -39,11 +39,9 @@ def find_pw(request):
 
 def find_mp(request):
 
-    str_name = request.GET.get("username")
-    str_phone = request.GET.get("phone")
+    str_name = request.POST.get("username")
+    str_phone = request.POST.get("phone")
     print('=============1111111==================')
-    if (str_name == "") or (str_phone == ""):
-        return HttpResponseRedirect("find_pw")
 
     print('===============================')
     print(str_name,str_phone)
@@ -52,23 +50,25 @@ def find_mp(request):
     conn = pymysql.connect(host='130.162.154.239', user='dev', password='1234', db='tilburg_club', charset='utf8')
     cur = conn.cursor()
     print('2')
-    sql_select = 'select password from userTable where username = (%s) and phone = (%s)'
+    sql_select = 'select userpassword from main where username = (%s) and phone = (%s)'
     val = (str_name, str_phone)
     cur.execute(sql_select, val)
+
+    if str_name != sql_select:
+        return render(request, "user/find_pw.html")
 
     print('3')
     row = cur.fetchone()
     str_password = row[0]
+
     print('4')
 
     content = f"<h1>{str_password} is your password</h1>"
 
     if (row == None):
         return render(request, "user/find_pw.html")
+
     print('5')
     return HttpResponse(content)
 
-    # if (str_name != "username") or (str_phone != "phone"):
-    #     return render(request, "user/find_pw.html")
-    # print('6')
-    #return render(request, "user/find_mp.html")
+
